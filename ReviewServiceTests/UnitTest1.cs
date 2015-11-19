75effbe2a652248f1791596ffe5ce6a225b92270
Peter Boyall <pboyall@hotmail.com>
2015-11-19 21:49:22 +0000

    Added Test Project

---
+++ ReviewServiceTests/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ReviewService;
+namespace ReviewServiceTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            ReviewService.Controllers.actiontask at = new ReviewService.Controllers.actiontask();
+            at.getTaskAssignment(2);
+
+
+        }
+    }
+}
